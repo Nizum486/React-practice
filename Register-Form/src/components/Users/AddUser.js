4207297f493d3af9fd4406befc47d82096e59046
Nizum486 <nizum486@gmail.com>
2023-03-28 20:25:11 +0900

    add Wrapper

--- Register-Form/src/components/Users/AddUser.js
+++ Register-Form/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 import Card from "../UI/Card"
 import Button from "../UI/Button"
 import ErrorModal from "../UI/ErrorModal"
+import Wrapper from "../Helper/Wrapper"
 
 import classes from "./AddUser.module.css"
 
@@ -57,7 +58,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && (
                 <ErrorModal
                     title={error.title}
@@ -85,7 +86,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add user</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     )
 }
 
