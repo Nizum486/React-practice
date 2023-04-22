dff70e09b5f08bb219ec5eb034b25ae5468d5fae
Nizum486 <nizum486@gmail.com>
2023-04-22 20:53:14 +0900

    feat : Meal List

--- Food-Order/src/App.js
+++ Food-Order/src/App.js
@@ -2,11 +2,16 @@ import { Fragment } from "react"
 
 import Header from "./components/Layout/Header"
 import Footer from "./components/Layout/Footer"
+import Meals from "./components/Meals/Meals"
+
 
 function App() {
     return (
         <Fragment>
             <Header />
+            <main>
+              <Meals/>
+            </main>
             <Footer />
         </Fragment>
     )
---
+++ Food-Order/src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>D food, D To You</h2>
+        <p>
+            여기에 문단이 들어옵니다.
+        </p>
+        <p>
+            여기에 문단이 들어옵니다.
+        </p>
+    </section>
+}
+
+export default MealsSummary
