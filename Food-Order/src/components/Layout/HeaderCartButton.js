50f78dc62c525322f2d6af3888d6419b07c0b3ea
Nizum486 <nizum486@gmail.com>
2023-05-03 15:41:16 +0900

    feat : context

--- Food-Order/src/components/Layout/HeaderCartButton.js
+++ Food-Order/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,16 @@
-import CartIcon from "../Cart/CartIcon"
+import { useContext } from "react"
 
+import CartIcon from "../Cart/CartIcon"
+import CartContext from "../../store/cart-context"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = (props) => {
+
+    const CartCtx = useContext(CartContext)
+
+    const numberofCartItems = CartCtx.items.length
+
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
